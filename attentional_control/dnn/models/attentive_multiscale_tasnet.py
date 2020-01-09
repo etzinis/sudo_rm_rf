3c4cd121fdbcad4809a34c9d168713706b516349
Efthymios Tzinis <etzinis2@illinois.edu>
2020-01-09 06:59:38 -0600

    Up and running deeply disentangled sources representations

--- attentional_control/dnn/models/attentive_multiscale_tasnet.py
+++ attentional_control/dnn/models/attentive_multiscale_tasnet.py
@@ -12,6 +12,96 @@ import os
 import glob2
 import datetime
 
+class Attention(nn.Module):
+    """ Applies attention mechanism on the `context` using the `query`.
+
+    **Thank you** to IBM for their initial implementation of :class:`Attention`. Here is
+    their `License
+    <https://github.com/IBM/pytorch-seq2seq/blob/master/LICENSE>`__.
+
+    Args:
+        dimensions (int): Dimensionality of the query and context.
+        attention_type (str, optional): How to compute the attention score:
+
+            * dot: :math:`score(H_j,q) = H_j^T q`
+            * general: :math:`score(H_j, q) = H_j^T W_a q`
+
+    Example:
+
+         >>> attention = Attention(256)
+         >>> query = torch.randn(5, 1, 256)
+         >>> context = torch.randn(5, 5, 256)
+         >>> output, weights = attention(query, context)
+         >>> output.size()
+         torch.Size([5, 1, 256])
+         >>> weights.size()
+         torch.Size([5, 1, 5])
+    """
+
+    def __init__(self, dimensions, attention_type='general'):
+        super(Attention, self).__init__()
+
+        if attention_type not in ['dot', 'general']:
+            raise ValueError('Invalid attention type selected.')
+
+        self.attention_type = attention_type
+        if self.attention_type == 'general':
+            self.linear_in = nn.Linear(dimensions, dimensions, bias=False)
+
+        self.linear_out = nn.Linear(dimensions * 2, dimensions, bias=False)
+        self.softmax = nn.Softmax(dim=-1)
+        self.tanh = nn.Tanh()
+
+    def forward(self, query, context):
+        """
+        Args:
+            query (:class:`torch.FloatTensor` [batch size, output length, dimensions]): Sequence of
+                queries to query the context.
+            context (:class:`torch.FloatTensor` [batch size, query length, dimensions]): Data
+                overwhich to apply the attention mechanism.
+
+        Returns:
+            :class:`tuple` with `output` and `weights`:
+            * **output** (:class:`torch.LongTensor` [batch size, output length, dimensions]):
+              Tensor containing the attended features.
+            * **weights** (:class:`torch.FloatTensor` [batch size, output length, query length]):
+              Tensor containing attention weights.
+        """
+        batch_size, output_len, dimensions = query.size()
+        query_len = context.size(1)
+
+        if self.attention_type == "general":
+            query = query.reshape(batch_size * output_len, dimensions)
+            query = self.linear_in(query)
+            query = query.reshape(batch_size, output_len, dimensions)
+
+        # TODO: Include mask on PADDING_INDEX?
+
+        # (batch_size, output_len, dimensions) * (batch_size, query_len, dimensions) ->
+        # (batch_size, output_len, query_len)
+        attention_scores = torch.bmm(query, context.transpose(1, 2).contiguous())
+
+        # Compute weights across every context sequence
+        attention_scores = attention_scores.view(batch_size * output_len, query_len)
+        attention_weights = self.softmax(attention_scores)
+        attention_weights = attention_weights.view(batch_size, output_len, query_len)
+
+        # (batch_size, output_len, query_len) * (batch_size, query_len, dimensions) ->
+        # (batch_size, output_len, dimensions)
+        mix = torch.bmm(attention_weights, context)
+
+        # concat -> (batch_size * output_len, 2*dimensions)
+        combined = torch.cat((mix, query), dim=2)
+        combined = combined.view(batch_size * output_len, 2 * dimensions)
+
+        # Apply linear_out on every 2nd dimension of concat
+        # output -> (batch_size, output_len, dimensions)
+        output = self.linear_out(combined).view(batch_size, output_len, dimensions)
+        output = self.tanh(output)
+
+        return output, attention_weights
+
+
 
 class TDCN(nn.Module):
 
@@ -71,7 +161,7 @@ class TDCN(nn.Module):
             TDCN.TCN(B=B, H=H, P=P, D=2 ** d)
             for _ in range(R) for d in range(X)])
 
-        if B * X * R != N:
+        if B != N:
             # self.ln_bef_out_reshape = GlobalLayerNorm(B)
             self.reshape_before_masks = nn.Conv1d(in_channels=B * X * R,
                                                   out_channels=N,
@@ -91,6 +181,14 @@ class TDCN(nn.Module):
                                      groups=S)
         # self.ln_mask_in = nn.BatchNorm1d(self.N)
         self.ln_mask_in = GlobalLayerNorm(self.N)
+        # self.attn = Attention((64000 // (self.L-1)))
+
+        # Masks layer
+        self.m2 = nn.Conv2d(in_channels=1,
+                            out_channels=S,
+                            kernel_size=(N + 1, 1),
+                            padding=(N - N // 2, 0))
+        self.ln_mask_in2 = GlobalLayerNorm(self.N)
 
     # Forward pass
     def forward(self, x):
@@ -105,27 +203,70 @@ class TDCN(nn.Module):
         x = self.ln(x)
         x = self.l1(x)
 
-        accum_output = []
+        accum_skip = []
+        accum_outs = []
 
         # do the forward path and also accumulate all scales representations
         for block in self.sm:
             x, interm_out = block(x)
-            accum_output.append(interm_out)
+            accum_skip.append(interm_out)
+            accum_outs.append(x)
 
-        x = torch.cat(accum_output, dim=1)
+        # final_hidden = x.clone()
+        x = torch.stack(accum_skip, dim=1)
+        # soft_scales = nn.functional.softmax(x, dim=1)
+        x = x.sum(dim=1)
 
-        if self.B * self.R * self.X != self.N:
+        if self.B != self.N:
             # x = self.ln_bef_out_reshape(x)
             x = self.reshape_before_masks(x)
 
         x = self.ln_mask_in(x)
         # Get masks and apply them
-        x = self.m(x.unsqueeze(1))
-        x = nn.functional.relu(x)
+        masks_vec = self.m(x.unsqueeze(1))
+        attn_vec = nn.functional.softmax(masks_vec, dim=1)
+
+        all_scales_outs = torch.stack(accum_outs, dim=1)
+        # print('MONOOO')
+        # print(all_scales_outs.shape)
+        # print(attn_vec.shape)
+        masked_1_scales_outs = (all_scales_outs * attn_vec[:, 0].unsqueeze(1)).sum(dim=1)
+        masked_2_scales_outs = (all_scales_outs * attn_vec[:, 1].unsqueeze(1)).sum(dim=1)
+
+        final_masks = torch.stack([masked_1_scales_outs,
+                                   masked_2_scales_outs], dim=1)
+
+        # print('PSOLA')
+        final_masks = self.ln_mask_in2(final_masks)
+        # print(final_masks.shape)
+        # Get masks and apply them
+        # final_masks = self.m2(final_masks.unsqueeze(1))
+        # print(final_masks.shape)
+
+        # print(final_masks.shape)
+
+        # print('Vectoras gia maskes')
+        # print(masks_vec.shape)
+        # print(soft_scales.shape)
+        # print('attention')
+        # query = masks_vec.view(soft_scales.shape[0],
+        #                        -1,
+        #                        (64000 // (self.L-1)))
+        # context = soft_scales.view(soft_scales.shape[0],
+        #                            -1,
+        #                            (64000 // (self.L-1)))
+        #
+        # attn_out, attn_weights = self.attn(query, context)
+        # # print(attn_out.max(), attn_out.min())
+        # # print(attn_out.shape)
+        # x = attn_out.view(soft_scales.shape[0], self.S, -1, (64000 // (self.L-1)))
+        # print('YOLO')
+        # print(x.shape)
+        # x = nn.functional.relu(x)
         if self.S == 1:
             x = torch.sigmoid(x)
         else:
-            x = nn.functional.softmax(x, dim=1)
+            x = nn.functional.softmax(final_masks, dim=1)
         x = x * s.unsqueeze(1)
         del s
 
@@ -332,7 +473,7 @@ if __name__ == "__main__":
         B=256,
         P=3,
         H=512,
-        R=1,
+        R=2,
         X=4,
         S=2,
         L=11,
