540cedee7049970b62d1d454cef386344cdd3572
Efthymios Tzinis <etzinis2@illinois.edu>
2020-04-01 16:34:39 -0500

    Create different files for CPU RAM utilization

--- attentional_control/utils/run_performance_measures.sh
+++ attentional_control/utils/run_performance_measures.sh
@@ -11,12 +11,23 @@ input_samples=8000
 for dev in cpu gpu; do
   for model in baseline_dprnn baseline_demucs baseline_twostep \
     sudormrf_R16 sudormrf_R8 sudormrf_R4 baseline_original_convtasnet; do \
-      /usr/bin/time -v /usr/bin/python3.6 extract_model_performance.py \
+       /usr/bin/python3.6 extract_model_performance.py \
       --device $dev --model_type $model --input_samples $input_samples \
       --batch_size $batch_size -cad $cuda_device --repeats 20 \
       --run_all \
-      > $out_dir/${model}_${dev}_bs_${batch_size}_samples_${input_samples} \
-      2>&1 && echo Processed $model with batch $batch_size and \
+      >> $out_dir/${model}_${dev}_bs_${batch_size}_samples_${input_samples} \
+      && \
+      /usr/bin/time -v /usr/bin/python3.6 extract_model_performance.py \
+      --device $dev --model_type $model --input_samples $input_samples \
+      --batch_size $batch_size -cad $cuda_device --repeats 1 \
+      --measure forward 2> \
+      $out_dir/forwardCPURAM${model}_${dev}_bs_${batch_size}_samples_${input_samples} \
+      && /usr/bin/time -v /usr/bin/python3.6 extract_model_performance.py \
+      --device $dev --model_type $model --input_samples $input_samples \
+      --batch_size $batch_size -cad $cuda_device --repeats 1 \
+      --measure backward 2> \
+      $out_dir/backwardCPURAM${model}_${dev}_bs_${batch_size}_samples_${input_samples} \
+      && echo Processed $model with batch $batch_size and \
       $input_samples samples on $dev;
   done;
 done
