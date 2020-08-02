6234ae7575e3c5a9a20a3787dc946d62e119ef8f
Efthymios Tzinis <etzinis2@illinois.edu>
2020-08-02 00:17:30 -0500

    Adding libri2mix path

--- __config__.py
+++ __config__.py
@@ -3,5 +3,6 @@ ESC50_DOWNLOADED_P = '/tmp/ESC-50-master'
 WSJ_MIX_HIERARCHICAL_P = '/mnt/nvme/hierarchical_sound_datasets/WSJ0_mix_partitioned/'
 ESC50_HIERARCHICAL_P = '/mnt/nvme/hierarchical_sound_datasets/ESC50_partitioned/'
 WHAM_ROOT_PATH = '/mnt/data/wham'
+LIBRI2MIX_ROOT_PATH = '/mnt/data/libri_mix/Libri2Mix'
 
-API_KEY = 'indert_cometml_key'
+API_KEY = 'your_cometml_api_key'
