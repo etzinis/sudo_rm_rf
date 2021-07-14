f4b178897476d8022c415d72f7cdbcdb5f6b75ea
Efthymios Tzinis <etzinis2@illinois.edu>
2021-07-14 17:40:22 -0500

    Dataset parser compatibility for FUSS

--- sudo_rm_rf/dnn/experiments/utils/dataset_setup.py
+++ sudo_rm_rf/dnn/experiments/utils/dataset_setup.py
@@ -1,13 +1,16 @@
 """!
 @brief Infer Dataset Specific parameters and return generators
-
 @author Efthymios Tzinis {etzinis2@illinois.edu}
 @copyright University of Illinois at Urbana-Champaign
 """
 
-from __config__ import WHAM_ROOT_PATH, LIBRI2MIX_ROOT_PATH
+from __config__ import WHAM_ROOT_PATH, LIBRI2MIX_ROOT_PATH, \
+    MUSDBWAV8K_ROOT_PATH, MUSDBWAV_ROOT_PATH, FUSS_ROOT_PATH
 import sudo_rm_rf.dnn.dataset_loader.libri2mix as libri2mix
 import sudo_rm_rf.dnn.dataset_loader.wham as wham_loader
+import sudo_rm_rf.dnn.dataset_loader.fuss as fuss_loader
+import sudo_rm_rf.dnn.dataset_loader.musdb_dataset as \
+    musdb_loader
 
 
 def create_loader_for_simple_dataset(dataset_name=None,
@@ -17,12 +20,20 @@ def create_loader_for_simple_dataset(dataset_name=None,
                                      min_or_max=None,
                                      zero_pad=None,
                                      timelegth=None,
+                                     n_channels=None,
                                      normalize_audio=None,
-                                     n_samples=None):
+                                     n_samples=None,
+                                     min_num_sources=None,
+                                     max_num_sources=None):
     if dataset_name == 'WHAM':
         loader = wham_loader
         root_path = WHAM_ROOT_PATH
         translator = {'train': 'tr', 'test': 'tt', 'val': 'cv'}
+        translated_split = translator[data_split]
+    elif dataset_name == 'FUSS':
+        loader = fuss_loader
+        root_path = FUSS_ROOT_PATH
+        translator = {'train': 'train', 'test': 'eval', 'val': 'validation'}
         translated_split = translator[data_split]
     elif dataset_name == 'LIBRI2MIX':
         loader = libri2mix
@@ -37,6 +48,15 @@ def create_loader_for_simple_dataset(dataset_name=None,
             translated_split = 'test'
         elif data_split == 'val':
             translated_split = 'dev'
+    elif dataset_name == 'MUSDB':
+        loader = musdb_loader
+        if sample_rate == 8000.:
+            root_path = MUSDBWAV8K_ROOT_PATH
+        elif sample_rate == 44100.:
+            root_path = MUSDBWAV_ROOT_PATH
+        else:
+            raise ValueError('Not appropriate sampling rate for MUSDB.')
+        translated_split = data_split
     else:
         raise ValueError('Dataset: {} is not yet supported!'.format(
             dataset_name))
@@ -44,9 +64,10 @@ def create_loader_for_simple_dataset(dataset_name=None,
     data_loader = loader.Dataset(
         root_dirpath=root_path, task=separation_task,
         split=translated_split, sample_rate=sample_rate, timelength=timelegth,
-        zero_pad=zero_pad, min_or_max=min_or_max,
+        zero_pad=zero_pad, min_or_max=min_or_max, n_channels=n_channels,
         augment='tr' in data_split,
-        normalize_audio=normalize_audio, n_samples=n_samples)
+        normalize_audio=normalize_audio, n_samples=n_samples,
+        min_num_sources=min_num_sources, max_num_sources=max_num_sources)
     return data_loader
 
 def setup(hparams):
@@ -66,11 +87,14 @@ def setup(hparams):
                     separation_task=hparams['separation_task'],
                     data_split=data_split.split('_')[0],
                     sample_rate=hparams['fs'],
+                    n_channels=hparams['n_channels'],
                     min_or_max=hparams['min_or_max'],
                     zero_pad=hparams['zero_pad_audio'],
                     timelegth=hparams['audio_timelength'],
                     normalize_audio=hparams['normalize_audio'],
-                    n_samples=hparams['n_'+data_split])
+                    n_samples=hparams['n_'+data_split],
+                    min_num_sources=hparams['min_num_sources'],
+                    max_num_sources=hparams['max_num_sources'])
         generators[data_split] = loader.get_generator(
             batch_size=hparams['batch_size'], num_workers=hparams['n_jobs'])
 
