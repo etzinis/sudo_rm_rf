f126c955a4f82610f334ebc1c0b872606aefda70
Efthymios Tzinis <etzinis2@illinois.edu>
2021-07-14 17:30:50 -0500

    Update all dataset paths including FUSS and MUSDB

--- __config__.py
+++ __config__.py
@@ -4,5 +4,9 @@ WSJ_MIX_HIERARCHICAL_P = '/mnt/nvme/hierarchical_sound_datasets/WSJ0_mix_partiti
 ESC50_HIERARCHICAL_P = '/mnt/nvme/hierarchical_sound_datasets/ESC50_partitioned/'
 WHAM_ROOT_PATH = '/mnt/data/wham'
 LIBRI2MIX_ROOT_PATH = '/mnt/data/libri_mix/Libri2Mix'
+MUSDB_ROOT_PATH = '/mnt/data/Music/MUSDB18'
+MUSDBWAV_ROOT_PATH = '/mnt/data/Music/MUSDB18wav'
+MUSDBWAV8K_ROOT_PATH = '/mnt/data/Music/MUSDB18wav8k'
+FUSS_ROOT_PATH = '/mnt/data/fuss_dataset/fuss_dev/ssdata'
 
-API_KEY = 'your_cometml_api_key'
+API_KEY = 'your_cometml_API_KEY'
