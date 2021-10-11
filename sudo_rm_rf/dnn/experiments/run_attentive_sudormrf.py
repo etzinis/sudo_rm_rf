ee57d9fd96b1aecb24eda5a1820a763eaf4682ab
etzinis <etzinis@gmail.com>
2021-10-11 14:32:05 -0500

    Adding attentive sudo experiment

--- sudo_rm_rf/dnn/experiments/run_attentive_sudormrf.py
+++ sudo_rm_rf/dnn/experiments/run_attentive_sudormrf.py
@@ -1,5 +1,5 @@
 """!
-@brief Running an experiment with the improved version of SuDoRmRf
+@brief Running an experiment with the Attentive-SuDoRmRf
 
 @author Efthymios Tzinis {etzinis2@illinois.edu}
 @copyright University of Illinois at Urbana-Champaign
@@ -18,11 +18,13 @@ import torch
 from torch.nn import functional as F
 from tqdm import tqdm
 from pprint import pprint
-import sudo_rm_rf.dnn.experiments.utils.improved_cmd_args_parser as parser
+import sudo_rm_rf.dnn.experiments.utils.improved_cmd_args_parser_v2 as parser
 import sudo_rm_rf.dnn.experiments.utils.dataset_setup as dataset_setup
 import sudo_rm_rf.dnn.losses.sisdr as sisdr_lib
+import sudo_rm_rf.dnn.experiments.utils.mixture_consistency as \
+    mixture_consistency
 import sudo_rm_rf.dnn.models.improved_sudormrf as improved_sudormrf
-import sudo_rm_rf.dnn.models.sudormrf as initial_sudormrf
+import sudo_rm_rf.dnn.models.attentive_sudormrf as attentive_sudormrf
 import sudo_rm_rf.dnn.utils.cometml_loss_report as cometml_report
 import sudo_rm_rf.dnn.utils.cometml_log_audio as cometml_audio_logger
 
@@ -56,13 +58,13 @@ os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(
 
 back_loss_tr_loss_name, back_loss_tr_loss = (
     'tr_back_loss_SISDRi',
-    # sisdr_lib.PITLossWrapper(sisdr_lib.PairwiseNegSDR("sisdr"),
-    #                          pit_from='pw_mtx')
-    sisdr_lib.PermInvariantSISDR(batch_size=hparams['batch_size'],
-                                 n_sources=hparams['n_sources'],
-                                 zero_mean=True,
-                                 backward_loss=True,)
-                                 # improvement=True)
+    sisdr_lib.PITLossWrapper(sisdr_lib.PairwiseNegSDR("sisdr"),
+                             pit_from='pw_mtx')
+    # sisdr_lib.PermInvariantSISDR(batch_size=hparams['batch_size'],
+    #                              n_sources=hparams['n_sources'],
+    #                              zero_mean=True,
+    #                              backward_loss=True,)
+    #                              # improvement=True)
 )
 
 val_losses = {}
@@ -86,14 +88,17 @@ if hparams['model_type'] == 'relu':
                                        enc_kernel_size=hparams['enc_kernel_size'],
                                        enc_num_basis=hparams['enc_num_basis'],
                                        num_sources=hparams['n_sources'])
-elif hparams['model_type'] == 'softmax':
-    model = initial_sudormrf.SuDORMRF(out_channels=hparams['out_channels'],
-                                      in_channels=hparams['in_channels'],
-                                      num_blocks=hparams['num_blocks'],
-                                      upsampling_depth=hparams['upsampling_depth'],
-                                      enc_kernel_size=hparams['enc_kernel_size'],
-                                      enc_num_basis=hparams['enc_num_basis'],
-                                      num_sources=hparams['n_sources'])
+elif hparams['model_type'] == 'attention':
+    model = attentive_sudormrf.SuDORMRF(out_channels=hparams['out_channels'],
+                                        in_channels=hparams['in_channels'],
+                                        num_blocks=hparams['num_blocks'],
+                                        upsampling_depth=hparams['upsampling_depth'],
+                                        enc_kernel_size=hparams['enc_kernel_size'],
+                                        enc_num_basis=hparams['enc_num_basis'],
+                                        n_heads=hparams['att_n_heads'],
+                                        att_dims=hparams['att_dims'],
+                                        att_dropout=hparams['att_dropout'],
+                                        num_sources=hparams['n_sources'])
 else:
     raise ValueError('Invalid model: {}.'.format(hparams['model_type']))
 
@@ -122,14 +127,17 @@ tr_step = 0
 val_step = 0
 prev_epoch_val_loss = 0.
 for i in range(hparams['n_epochs']):
+    batch_step = 0
+    sum_loss = 0.
     res_dic = {}
     for loss_name in all_losses:
         res_dic[loss_name] = {'mean': 0., 'std': 0., 'acc': []}
-    print("Improved Sudo-RM-RF: {} - {} || Epoch: {}/{}".format(
+    print("Attentive Sudo-RM-RF: {} - {} || Epoch: {}/{}".format(
         experiment.get_key(), experiment.get_tags(), i+1, hparams['n_epochs']))
     model.train()
 
-    for data in tqdm(generators['train'], desc='Training'):
+    training_gen_tqdm = tqdm(generators['train'], desc='Training')
+    for data in training_gen_tqdm:
         opt.zero_grad()
         clean_wavs = data[-1].cuda()
         m1wavs = data[0].cuda()
@@ -151,16 +159,27 @@ for i in range(hparams['n_epochs']):
         # ===============================================
 
         rec_sources_wavs = model(m1wavs.unsqueeze(1))
+        # rec_sources_wavs = mixture_consistency.apply(
+        #     rec_sources_wavs, m1wavs.unsqueeze(1)
+        # )
 
-        l = back_loss_tr_loss(rec_sources_wavs,
-                              clean_wavs)
+        l = torch.clamp(
+            back_loss_tr_loss(rec_sources_wavs, clean_wavs),
+            min=-30., max=+30.)
         l.backward()
         if hparams['clip_grad_norm'] > 0:
             torch.nn.utils.clip_grad_norm_(model.parameters(),
                                            hparams['clip_grad_norm'])
 
         opt.step()
-    # lr_scheduler.step(res_dic['val_SISDRi']['mean'])
+
+        np_loss_value = l.detach().item()
+        sum_loss += np_loss_value
+        training_gen_tqdm.set_description(
+            f"Training, Running Avg Loss: {sum_loss / (batch_step + 1)}"
+        )
+        batch_step += 1
+
     if hparams['patience'] > 0:
         if tr_step % hparams['patience'] == 0:
             new_lr = (hparams['learning_rate']
@@ -181,6 +200,9 @@ for i in range(hparams['n_epochs']):
                     clean_wavs = data[-1].cuda()
 
                     rec_sources_wavs = model(m1wavs.unsqueeze(1))
+                    # rec_sources_wavs = mixture_consistency.apply(
+                    #     rec_sources_wavs, m1wavs.unsqueeze(1)
+                    # )
 
                     for loss_name, loss_func in val_losses[val_set].items():
                         l = loss_func(rec_sources_wavs,
@@ -188,7 +210,9 @@ for i in range(hparams['n_epochs']):
                                       initial_mixtures=m1wavs.unsqueeze(1))
                         res_dic[loss_name]['acc'] += l.tolist()
 
-            audio_logger.log_batch(rec_sources_wavs, clean_wavs, m1wavs,
+            audio_logger.log_batch(rec_sources_wavs[:2],
+                                   clean_wavs[:2],
+                                   m1wavs[:2],
                                    experiment, step=val_step, tag=val_set)
 
     val_step += 1
