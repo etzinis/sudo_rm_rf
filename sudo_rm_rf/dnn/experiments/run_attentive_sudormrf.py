841c33ca66f993ab27c6988577c6665f9120b7bf
Efthymios Tzinis <etzinis2@illinois.edu>
2021-10-13 18:57:06 -0500

    Improved attentional sudo model

--- sudo_rm_rf/dnn/experiments/run_attentive_sudormrf.py
+++ sudo_rm_rf/dnn/experiments/run_attentive_sudormrf.py
@@ -25,6 +25,7 @@ import sudo_rm_rf.dnn.experiments.utils.mixture_consistency as \
     mixture_consistency
 import sudo_rm_rf.dnn.models.improved_sudormrf as improved_sudormrf
 import sudo_rm_rf.dnn.models.attentive_sudormrf as attentive_sudormrf
+import sudo_rm_rf.dnn.models.attentive_sudomrf_v2 as attentive_sudomrf_v2
 import sudo_rm_rf.dnn.utils.cometml_loss_report as cometml_report
 import sudo_rm_rf.dnn.utils.cometml_log_audio as cometml_audio_logger
 
@@ -89,6 +90,17 @@ if hparams['model_type'] == 'relu':
                                        enc_num_basis=hparams['enc_num_basis'],
                                        num_sources=hparams['n_sources'])
 elif hparams['model_type'] == 'attention':
+    model = attentive_sudormrf.SuDORMRF(out_channels=hparams['out_channels'],
+                                        in_channels=hparams['in_channels'],
+                                        num_blocks=hparams['num_blocks'],
+                                        upsampling_depth=hparams['upsampling_depth'],
+                                        enc_kernel_size=hparams['enc_kernel_size'],
+                                        enc_num_basis=hparams['enc_num_basis'],
+                                        n_heads=hparams['att_n_heads'],
+                                        att_dims=hparams['att_dims'],
+                                        att_dropout=hparams['att_dropout'],
+                                        num_sources=hparams['n_sources'])
+elif hparams['model_type'] == 'attention_v2':
     model = attentive_sudormrf.SuDORMRF(out_channels=hparams['out_channels'],
                                         in_channels=hparams['in_channels'],
                                         num_blocks=hparams['num_blocks'],
