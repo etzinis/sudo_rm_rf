ccf193b513ef490167fa8de9fa16b7e15604e23a
etzinis <etzinis@gmail.com>
2022-02-26 15:17:12 -0600

    Adding zenodo file downloader

---
+++ pretrained_models/download_pretrained_models.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://zenodo.org/record/6299852/files/GroupCom_Sudormrf_U8_Bases512_WSJ02mix.pt?download=1 -O GroupCom_Sudormrf_U8_Bases512_WSJ02mix.pt
+wget https://zenodo.org/record/6299852/files/Improved_Sudormrf_U16_Bases2048_WHAMRexclmark.pt?download=1 -O Improved_Sudormrf_U16_Bases2048_WHAMRexclmark.pt
+wget https://zenodo.org/record/6299852/files/Improved_Sudormrf_U16_Bases512_WSJ02mix.pt?download=1 -O Improved_Sudormrf_U16_Bases512_WSJ02mix.pt
+wget https://zenodo.org/record/6299852/files/Improved_Sudormrf_U36_Bases2048_WSJ02mix.pt?download=1 -O Improved_Sudormrf_U36_Bases2048_WSJ02mix.pt
+wget https://zenodo.org/record/6299852/files/Improved_Sudormrf_U36_Bases4096_WHAMRexclmark.pt?download=1 -O Improved_Sudormrf_U36_Bases4096_WHAMRexclmark.pt
